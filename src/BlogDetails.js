3b53b7508ba6a8ce78b434fdf67774aa308daed4
roman.syed <romansyed008@gmail.com>
2024-06-04 11:18:01 +0600

    blog create and delete

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,24 +1,34 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 import { useEffect } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const {data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/'+id);
+    const history = useHistory();
 
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/'+blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
     // useEffect(() => {
     //     console.log(blog);
     //   }, [blog]);
 
     return ( 
         <div className="blog-details">
-            { isPending && <div> Loding... </div> }
+            { isPending && <div> Loading... </div> }
             { error && <div>{error}</div> }
             {blog && (
                 <article>
                     <h2>{ blog.title }</h2>
                     <p>Written by: {blog.author}</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
 
