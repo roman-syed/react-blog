fb8d29fed1916440a947a186e4cda9ce105f8f2e
roman.syed <romansyed008@gmail.com>
2024-06-04 11:25:13 +0600

    error page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Create from './Create';
 import Home from './Home';
 import Navbar from "./Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NotFound from './NotFound';
 
 function App() {
 
@@ -20,6 +21,9 @@ function App() {
               </Route>
               <Route exact path="/blogs/:id">
                 <BlogDetails/>
+              </Route>
+              <Route path="*">
+                <NotFound/>
               </Route>
             </Switch>
           </div>
